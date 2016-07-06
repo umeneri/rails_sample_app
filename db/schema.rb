0255a393c13328e882047a6603e9574596c362c8
umeneri <mokushiroku97@gmail.com>
2016-07-06 21:50:39 +0900

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,13 @@ ActiveRecord::Schema.define(version: 20160701034459) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
     t.string   "activation_digest"
-    t.boolean  "activated"
+    t.boolean  "activated",         default: false
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
